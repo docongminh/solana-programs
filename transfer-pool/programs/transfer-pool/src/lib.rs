60cfa1f23c6761f2dbc84ac91d3a588f801719e3
docongminh <cristiando0902@gmail.com>
2022-08-06 22:12:31 +0700

    UPDATE: withdraw

--- transfer-pool/programs/transfer-pool/src/lib.rs
+++ transfer-pool/programs/transfer-pool/src/lib.rs
@@ -57,67 +57,67 @@ pub mod transfer {
         Ok(())
     }
 
-    // pub fn with_draw(
-    //     ctx: Context<WithDrawInstruction>,
-    //     state_bump: u8,
-    //     wallet_bump: u8,
-    // ) -> Result<()> {
-    //     let current_stage = Stage::from(ctx.accounts.state_account.stage)?;
-    //     let is_valid_stage = current_stage == Stage::Deposit || current_stage == Stage::WithDraw;
-    //     if !is_valid_stage {
-    //         return Err(ErrorCode::InvalidStage.into());
-    //     }
+    pub fn with_draw(
+        ctx: Context<WithDrawInstruction>,
+        state_bump: u8,
+        wallet_bump: u8,
+    ) -> Result<()> {
+        let current_stage = Stage::from(ctx.accounts.state_account.stage)?;
+        let is_valid_stage = current_stage == Stage::Deposit || current_stage == Stage::WithDraw;
+        if !is_valid_stage {
+            return Err(ErrorCode::InvalidStage.into());
+        }
 
-    //     let bump_vector = state_bump.to_le_bytes();
-    //     let mint_token = ctx.accounts.mint.key().clone();
-    //     let inner = vec![
-    //         b"state".as_ref(),
-    //         ctx.accounts.user.key.as_ref(),
-    //         mint_token.as_ref(),
-    //         bump_vector.as_ref(),
-    //     ];
-    //     let outer = vec![inner.as_slice()];
+        let bump_vector = state_bump.to_le_bytes();
+        let mint_token = ctx.accounts.mint.key().clone();
+        let inner = vec![
+            b"state".as_ref(),
+            ctx.accounts.user.key.as_ref(),
+            mint_token.as_ref(),
+            bump_vector.as_ref(),
+        ];
+        let outer = vec![inner.as_slice()];
 
-    //     let transfer_instruction = Transfer {
-    //         from: ctx
-    //             .accounts
-    //             .escrow_wallet_associate_account
-    //             .to_account_info(),
-    //         to: ctx.accounts.user_associated_account.to_account_info(),
-    //         authority: ctx.accounts.state_account.to_account_info(),
-    //     };
-    //     let cpi_ctx = CpiContext::new_with_signer(
-    //         ctx.accounts.token_program.to_account_info(),
-    //         transfer_instruction,
-    //         outer.as_slice(),
-    //     );
-    //     anchor_spl::token::transfer(cpi_ctx, ctx.accounts.escrow_wallet_associate_account.amount)?;
+        let transfer_instruction = Transfer {
+            from: ctx
+                .accounts
+                .escrow_wallet_associate_account
+                .to_account_info(),
+            to: ctx.accounts.user_associated_account.to_account_info(),
+            authority: ctx.accounts.state_account.to_account_info(),
+        };
+        let cpi_ctx = CpiContext::new_with_signer(
+            ctx.accounts.token_program.to_account_info(),
+            transfer_instruction,
+            outer.as_slice(),
+        );
+        anchor_spl::token::transfer(cpi_ctx, ctx.accounts.escrow_wallet_associate_account.amount)?;
 
-    //     // close Account when dont use any where again
-    //     let should_close = {
-    //         ctx.accounts.escrow_wallet_associate_account.reload()?;
-    //         ctx.accounts.escrow_wallet_associate_account.amount == 0
-    //     };
+        // close Account when dont use any where again
+        let should_close = {
+            ctx.accounts.escrow_wallet_associate_account.reload()?;
+            ctx.accounts.escrow_wallet_associate_account.amount == 0
+        };
 
-    //     if should_close {
-    //         let ca = CloseAccount {
-    //             account: ctx
-    //                 .accounts
-    //                 .escrow_wallet_associate_account
-    //                 .to_account_info(),
-    //             destination: ctx.accounts.user.to_account_info(),
-    //             authority: ctx.accounts.state_account.to_account_info(),
-    //         };
-    //         let cpi_ctx = CpiContext::new_with_signer(
-    //             ctx.accounts.token_program.to_account_info(),
-    //             ca,
-    //             outer.as_slice(),
-    //         );
-    //         anchor_spl::token::close_account(cpi_ctx)?;
-    //     }
+        if should_close {
+            let ca = CloseAccount {
+                account: ctx
+                    .accounts
+                    .escrow_wallet_associate_account
+                    .to_account_info(),
+                destination: ctx.accounts.user.to_account_info(),
+                authority: ctx.accounts.state_account.to_account_info(),
+            };
+            let cpi_ctx = CpiContext::new_with_signer(
+                ctx.accounts.token_program.to_account_info(),
+                ca,
+                outer.as_slice(),
+            );
+            anchor_spl::token::close_account(cpi_ctx)?;
+        }
 
-    //     Ok(())
-    // }
+        Ok(())
+    }
 }
 
 #[account]
@@ -165,34 +165,34 @@ pub struct DepositInstruction<'info> {
     token_program: Program<'info, Token>,
 }
 
-// #[derive(Accounts)]
-// #[instruction(state_bump: u8, wallet_bump: u8)]
-// pub struct WithDrawInstruction<'info> {
-//     #[account(
-//         mut,
-//         seeds=[b"state".as_ref(), user.key().as_ref(), mint.key().as_ref()],
-//         bump = state_bump,
-//         has_one = user,
-//         has_one = mint,
-//     )]
-//     state_account: Account<'info, State>,
-//     #[account(
-//         mut,
-//         seeds=[b"wallet".as_ref(), user.key().as_ref(), mint.key().as_ref()],
-//         bump = wallet_bump,
-//     )]
-//     escrow_wallet_associate_account: Account<'info, TokenAccount>,
-//     #[account(mut)]
-//     user: Signer<'info>,
-//     mint: Account<'info, Mint>,
-//     // refund wallet
-//     #[account(
-//         mut,
-//         constraint=user_associated_account.owner == user.key(),
-//         constraint=user_associated_account.mint == mint.key()
-//     )]
-//     user_associated_account: Account<'info, TokenAccount>,
-//     // system
-//     system_program: Program<'info, System>,
-//     token_program: Program<'info, Token>,
-// }
+#[derive(Accounts)]
+#[instruction(state_bump: u8, wallet_bump: u8)]
+pub struct WithDrawInstruction<'info> {
+    #[account(
+        mut,
+        seeds=[b"state".as_ref(), user.key().as_ref(), mint.key().as_ref()],
+        bump = state_bump,
+        has_one = user,
+        has_one = mint,
+    )]
+    state_account: Account<'info, State>,
+    #[account(
+        mut,
+        seeds=[b"wallet".as_ref(), user.key().as_ref(), mint.key().as_ref()],
+        bump = wallet_bump,
+    )]
+    escrow_wallet_associate_account: Account<'info, TokenAccount>,
+    #[account(mut)]
+    user: Signer<'info>,
+    mint: Account<'info, Mint>,
+    // refund wallet
+    #[account(
+        mut,
+        constraint=user_associated_account.owner == user.key(),
+        constraint=user_associated_account.mint == mint.key()
+    )]
+    user_associated_account: Account<'info, TokenAccount>,
+    // system
+    system_program: Program<'info, System>,
+    token_program: Program<'info, Token>,
+}
